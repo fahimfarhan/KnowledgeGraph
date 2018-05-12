f763690a6f1ba44ab350ef331cdea0f8c8b63f39
fahimfarhan <fahim.farhan@outlook.com>
2018-05-13 00:43:41 +0600

    FC1

---
+++ start.py
@@ -0,0 +1,18 @@
+# consumer key, consumer secret, access token, access secret.
+from tweepy import OAuthHandler, Stream
+from twitter.listener import listener
+from decouple import config
+
+ckey = config('ckey')
+csecret = config('csecret')
+atoken = config('atoken')
+asecret = config('asecret')
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+
+twitterStream = Stream(auth, listener())
+# twitterStream.filter(track=["infinitywar"])
+twitterStream.filter(track=["myfirstTweet", "car", "messi", "farcry", "Bangladesh", "ronaldo", "assassinscreed",
+                            "china", "usa", "metoo"])
+# twitterStream.all()
